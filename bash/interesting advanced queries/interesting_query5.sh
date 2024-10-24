132548d31768da586891ac35d82b01d961e35563
Joe0427 <121193809+Joe0427@users.noreply.github.com>
2024-10-23 20:57:09 -0400

    Update interesting_query5.sh
    I changed it to exists and yellow cards bcs the result table is much much smaller and a screenshot can easily be taken

--- bash/interesting advanced queries/interesting_query5.sh
+++ bash/interesting advanced queries/interesting_query5.sh
@@ -3,18 +3,20 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
--- 5) Get players who receive no red cards using the NOT EXISTS operator.
-
+/*
+5) Get players who received yellow cards using the EXISTS operator.
+*/
 SELECT p.PlayerID, p.FirstName, p.LastName, t.TeamName
 FROM Player p
 JOIN Team t ON p.TeamID = t.TeamID
-WHERE NOT EXISTS (
+WHERE EXISTS (
     SELECT *
     FROM GamePlayerStats gps
     JOIN Player p2 ON gps.PlayerID = p2.PlayerID
     WHERE p2.TeamID = t.TeamID
-    AND gps.RedCards > 0
+    AND gps.PlayerYellowCards > 0
 );
+
 
 
 exit;
