9ad9ea16fba8f5602ae16e0d5e1f71632d97c43d
Joe0427 <121193809+Joe0427@users.noreply.github.com>
2024-10-23 20:11:23 -0400

    Update interesting_query1.sh
    query 5

--- bash/interesting advanced queries/interesting_query1.sh
+++ bash/interesting advanced queries/interesting_query1.sh
@@ -3,6 +3,16 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
+/* 
+5) A special trophy is given to those who got the most amount of hattricks or higher (3 goals or more in a single game). 
+The competition Admin (Joseph Guirguis) wants to find who got the most amount of hattricks (more than 3 in a game counts as a hattrick).
+*/
+
+SELECT p.PlayerID, p.FirstName, p.LastName, p.PlayerPosition, t.TeamName,COUNT(gps.PlayerID) AS hattricks
+FROM GamePlayerStats gps, Player p, Game g, Team t
+WHERE p.PlayerID = gps.PlayerID AND gps.PlayerGoals >= 3 AND g.GameID = gps.GameID AND p.TeamID = t.TeamID
+GROUP BY p.PlayerID, p.FirstName, p.LastName, p.PlayerPosition, t.TeamName
+ORDER BY hattricks DESC;
 
 exit;
 EOF
